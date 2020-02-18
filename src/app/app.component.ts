0164a814c8f0656e54c8ea553d71449a3ad82710
JOSEPH HEANEY - STUDENT <G00369134@gmit.ie>
2020-02-18 17:46:17 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+
+  count:number = 0;
+
+  hideLabel:boolean = true;
+
+  buttonPressed(){
+    console.log("Button has been Pressed");
+    this.count++;
+  }
+
+  starPressed(){
+    console.log("Star has been double clicked");
+    if(this.hideLabel == true){
+      this.hideLabel = false;
+    } else{
+      this.hideLabel = true;
+    }   
+  }
+  
 }
